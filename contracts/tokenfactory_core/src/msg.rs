54eb3bd4e32a4a34900911b81aec1c9b387004f7
Ben | Monarch <bgoober2@protonmail.com>
2023-03-27 23:52:00 -0400

    Multiple edits
    Signed-off-by: Ben | Monarch <bgoober2@protonmail.com>

--- contracts/tokenfactory_core/src/msg.rs
+++ contracts/tokenfactory_core/src/msg.rs
@@ -5,8 +5,8 @@ pub struct InstantiateMsg {
     // the manager of the contract is the one who can transfer the admin to another address
     // Typically this should be a multisig or a DAO (https://daodao.zone/)
     // Default is the contract initializer
-    pub manager: Option<String>,
-    pub allowed_mint_addresses: Vec<String>,
+    pub manager: Option<String>, // internal manager of the contract. Different from the external admin passed in the --admin flag from the cli during instantiation.
+    pub allowed_mint_addresses: Vec<String>, // addresses allowed to pass the ExecuteMsg::Mint to this contract. This would be your contract's address.
 
     // We can manage multiple denoms
     pub denoms: Vec<String>, // ex: factory/juno1xxx/test
--- contracts/tokenfactory_core/src/state.rs
+++ contracts/tokenfactory_core/src/state.rs
@@ -3,9 +3,9 @@ use cw_storage_plus::Item;
 
 #[cw_serde]
 pub struct Config {
-    pub manager: String,
-    pub allowed_mint_addresses: Vec<String>,
-    pub denoms: Vec<String>,
+    pub manager: String, // an internal admin or manager of the contract, not the same as the --admin flag passed during the instantiation of the contract
+    pub allowed_mint_addresses: Vec<String>, // addresses that are allowed to pass the ExecuteMsg::Mint to this contract. This would be your contract's address
+    pub denoms: Vec<String>, // denomination of the native token that this contract manages the minting of
 }
 
 pub const STATE: Item<Config> = Item::new("config");
